50d3bb123b242c9537e2d2015c9838e364c8a9ed
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2026-04-19 15:42:21 -0500

    feat(tracking): add Meta Pixel (1875719876442536) with CSP allowlist

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -173,6 +173,29 @@ export default async function LocaleLayout({ children, params }: Props) {
         src="//cdn.callrail.com/companies/413132698/334d41c65d6e3acabf78/12/swap.js"
         strategy="lazyOnload"
       />
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1875719876442536');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1875719876442536&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
     </html>
   );
 }
